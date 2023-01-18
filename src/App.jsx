51b9b1046f8f3aa1d275b3ca85bde5d54c99966d
imskanand <the.anand3099@gmail.com>
2023-01-19 00:15:50 +0530

    Mapping through the data Animals

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import {createRoot} from 'react-dom/client';
-import SearchParams from './SearchParams';
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <>
       <h1>Adopt Me!</h1>
-      <SearchParams/>
+      <SearchParams />
     </>
-  )
+  );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App/>);
+root.render(<App />);
--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,11 +1,11 @@
 const Pet = (props) => {
   return (
     <>
-      <h1>{ props.name }</h1>
-      <h1>{ props.animal }</h1>
-      <h1>{ props.breed }</h1>
+      <h1>{props.name}</h1>
+      <h1>{props.animal}</h1>
+      <h1>{props.breed}</h1>
     </>
-  )
+  );
 };
 
-export default Pet
+export default Pet;
