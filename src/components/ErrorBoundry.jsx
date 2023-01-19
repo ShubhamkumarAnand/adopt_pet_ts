6fe3351fc5a06e061251e9b676fae46ecc7da352
imskanand <the.anand3099@gmail.com>
2023-01-19 23:32:21 +0530

    Error Collection and Defining Error Boundary

---
+++ src/components/ErrorBoundry.jsx
@@ -0,0 +1,30 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    // TrackJs or NewRelic
+    console.error("ErrorBoundary component caught an Error", error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div>
+          <h2>
+            There was an Error with this listing.{" "}
+            <Link to="/">Click Here to Go Back to Home Page</Link>
+          </h2>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
