81ef92d5a6724447e081ca1fb51bc40d6d0af4f6
imskanand <the.anand3099@gmail.com>
2023-01-20 14:18:20 +0530

    Added context on the details page

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,14 +1,18 @@
-import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useState, useContext } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 
 import fetchPet from "../utils/fetchPet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import Modal from "./Modal";
+import AdoptedCreateContext from "../context/AdoptedPetContext";
 
 const Details = () => {
   const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
+  // eslint-disable-next-line no-unused-vars
+  const [_, setAdoptedPet] = useContext(AdoptedCreateContext);
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
 
@@ -38,7 +42,14 @@ const Details = () => {
             <div>
               <h1>Would you like to Adopt the {pet.name}</h1>
               <div className="buttons">
-                <button>Yes</button>
+                <button
+                  onClick={() => {
+                    setAdoptedPet(pet);
+                    navigate("/");
+                  }}
+                >
+                  Yes
+                </button>
                 <button onClick={() => setShowModal(false)}>No</button>
               </div>
             </div>
