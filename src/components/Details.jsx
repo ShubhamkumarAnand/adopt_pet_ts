581f4336284cbc847b6611f769d405834616b27a
imskanand <the.anand3099@gmail.com>
2023-01-20 13:56:51 +0530

    Implementing Modals with portals

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 
 import fetchPet from "../utils/fetchPet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
 
 const Details = () => {
+  const [showModal, setShowModal] = useState(false);
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
 
@@ -26,8 +29,21 @@ const Details = () => {
         <h2>
           {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
         </h2>
-        <button>Adopt Me</button>
+        <button onClick={() => setShowModal(true)}>
+          Adopt Me : {pet.name}
+        </button>
         <p>{pet.description}</p>
+        {showModal ? (
+          <Modal>
+            <div>
+              <h1>Would you like to Adopt the {pet.name}</h1>
+              <div className="buttons">
+                <button>Yes</button>
+                <button onClick={() => setShowModal(false)}>No</button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     </div>
   );
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -11,7 +11,7 @@ const Modal = ({ children }) => {
     modalRoot.appendChild(elRef.current);
     return () => modalRoot.removeChild(elRef.current);
   }, []);
-  return createPortal(<div>{children}</div>);
+  return createPortal(<div>{children}</div>, elRef.current);
 };
 
 export default Modal;
