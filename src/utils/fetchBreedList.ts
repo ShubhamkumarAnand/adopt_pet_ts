6572e159487bb9fd01e60bc8456e3bd6e3a60060
imskanand <the.anand3099@gmail.com>
2023-01-30 01:03:36 +0530

    Refactoring the fetch function

--- src/utils/fetchBreedList.ts
+++ src/utils/fetchBreedList.ts
@@ -1,6 +1,12 @@
-const fetchBreedList = async ({ queryKey }) => {
+import { QueryFunction } from "@tanstack/react-query"
+
+import { Animal, BreedListAPIResponse } from "./APIResponsesTypes"
+
+const fetchBreedList: QueryFunction<
+  BreedListAPIResponse,
+  ["breeds", Animal]
+> = async ({ queryKey }) => {
   const animal = queryKey[1]
-  if (!animal) return []
   const apiRes = await fetch(
     `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
   )
